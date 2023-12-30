9405cf7b64ab8244653828c980414bd7ce0c4323
Tim Honermann <honermti@windowslive.com>
2023-12-30 01:57:07 +0100

    fix: map null category correctly

--- backend/src/main/java/com/bookishlist/backend/lookup/mappers/LookupItemMapper.java
+++ backend/src/main/java/com/bookishlist/backend/lookup/mappers/LookupItemMapper.java
@@ -4,6 +4,8 @@ import com.bookishlist.backend.lookup.dtos.IndustryIdentifier;
 import com.bookishlist.backend.lookup.dtos.IndustryIdentifierType;
 import com.bookishlist.backend.lookup.dtos.LookupItem;
 import com.bookishlist.backend.lookup.dtos.LookupItemResponse;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import org.springframework.stereotype.Component;
 
@@ -20,11 +22,14 @@ public class LookupItemMapper implements Function<LookupItemResponse, LookupItem
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException("No ISBN found"));
 
+        var category =
+            Objects.requireNonNullElse(itemResponse.volumeInfo().categories(), List.of(""));
+
         return new LookupItem(
             isbn,
             itemResponse.volumeInfo().title(),
             String.join(", ", itemResponse.volumeInfo().authors()),
-            String.join(", ", itemResponse.volumeInfo().categories()),
+            String.join(", ", category),
             itemResponse.volumeInfo().imageLinks().thumbnail()
         );
     }
